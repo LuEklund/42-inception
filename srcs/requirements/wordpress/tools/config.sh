33b66d1fd5bc2fd0875776844134cd8d2d454250
LuEklund <luckeeklund@gmail.com>
2023-10-06 20:46:00 +0300

    cleaner and more compact wordpress handeling

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,32 +1,28 @@
-#!/bin/bash
+#! /bin/bash
 
-# create directory to use in nginx container later and also to setup the wordpress conf
-mkdir /var/www/
-mkdir /var/www/html
+chown -R www-data:www-data /var/www/html
+chmod -R 775 /var/www/html
 
-cd /var/www/html
+# Establish connection to mariadb before proceeding
+while ! mariadb -h${DB_HOST} -u${DB_USER} -p${DB_PASSWORD} ${DB_NAME} &>/dev/null; do
+		echo Establishing connection to database..
+    sleep 5
+done
 
-rm -rf *
 
-#execute only if wordpress is not installed
-if [ ! -f /var/www/html/wp-config.php ]; then
-
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
-	chmod +x wp-cli.phar 
-
-	mv wp-cli.phar /usr/local/bin/wp
+if [ -f "wp-config.php" ]; then
+	echo "WordPress: already installed"
+else
 
 	wp core download --allow-root
+	echo $?
+	echo "========================\nCHECKED THE STAUS OF WP\n=================================="
 
-	#setup wp-config.php file with the proper values
-	sed -i -r "s/database_name_here/$DB_NAME/1"   /var/www/html/wp-config-sample.php
-	sed -i -r "s/username_here/$DB_USER/1"  /var/www/html/wp-config-sample.php
-	sed -i -r "s/password_here/$DB_PASSWORD/1"    /var/www/html/wp-config-sample.php
-	sed -i -r "s/localhost/$DB_HOST/1"    /var/www/html/wp-config-sample.php
-
-
-	cp wp-config-sample.php wp-config.php
+	wp config create --allow-root \
+			--dbhost=${DB_HOST} \
+			--dbname=${DB_NAME} \
+			--dbuser=${DB_USER} \
+			--dbpass=${DB_PASSWORD}
 
 	#install and configure wordpress and create a new user
 	wp core install --url=${DOMAIN_NAME} \
@@ -42,11 +38,12 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 					--role=author \
 					--user_pass=$WP_USER_PASSWORD \
 					--allow-root
-
+	
 	#Install a theme
 	wp theme install inspiro --activate --allow-root
+	echo Finished installation and setup!
 
 fi
 
 #start the PHP-FPM (FastCGI Process Manager) service for PHP version 7.4 in foreground mode
-/usr/sbin/php-fpm7.4 -F
+/usr/sbin/php-fpm7.4 -R -F
